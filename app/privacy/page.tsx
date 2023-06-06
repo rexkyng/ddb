6cf63f0982826290136479ee6e474984975ef4a6
timescam <timescam@duck.com>
2023-06-06 16:43:42 +0800

    initial dump, url works but ui needed more work

---
+++ app/privacy/page.tsx
@@ -0,0 +1,40 @@
+import { Heading, Paragraph, Box, Text, heights } from "dracula-ui";
+import HR from "../components/HR";
+
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<Box
+				mb="sm"
+				className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"
+			>
+				<Box mb="sm">
+					<Heading size="2xl">DuckDuckBing</Heading>
+				</Box>
+				{/* <Box mb="sm">
+					<Divider color="animated" />
+				</Box> */}
+				<HR />
+				<Box mb="sm">
+					<Heading size="lg">Privacy Policy</Heading>
+				</Box>
+				<Box mb="sm">
+					<Paragraph>
+						It's quite simple. DuckDuckBing doesn't track any
+						queries submitted whatsoever, It simply redirects you to
+						DuckDuckGo or Bing depending on whether your search
+						contains a !bang or not.
+					</Paragraph>
+					<Paragraph>
+						The source code for this is available{" "}
+						<a href="https://github.com/timescam/duckduckbing/blob/master/app.js#L22">
+							here on GitHub
+						</a>{" "}
+						so you're free to take a look or even host your own
+						version if you'd like.
+					</Paragraph>
+				</Box>
+			</Box>
+		</main>
+	);
+}
---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextURL } from "next/dist/server/web/next-url";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+	if (request.nextUrl.searchParams.get("q") === null) {
+		return NextResponse.next();
+	} else if (request.nextUrl.searchParams.get("q")?.startsWith("!")) {
+		return NextResponse.redirect(
+			"https://duckduckgo.com/?q=" + request.nextUrl.searchParams.get("q")
+		);
+	} else {
+		return NextResponse.redirect(
+			"https://google.com/?search=" +
+				request.nextUrl.searchParams.get("q")
+		);
+	}
+}
